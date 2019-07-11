1e5c404d347dc4774faf300c772421776acd86b6
farid LKHALDOUNI <k.modor@yahoo.com>
2019-07-11 17:19:22 +0200

    add background image

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Farid LKHALDOUNI</a>
+    <div class="d-flex flex-column">
+        <a class="navbar-brand" href="#">Farid LKHALDOUNI</a>
+        <span>Web developpeur</span>
+    </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.header')
 
 @section('content')
-    <h1>Welcome page</h1>
+<div class="image-wrap">
+    <img class="bg-w100-max-height" src="/images/bg-pencils.jpg">
+    <span class="text-white centered-text">DEVELOPPEUR WEB FULL STACK</span>
+</div>
+    
 @endsection
 
 @extends('layouts.footer')
+
+<style type="text/css">
+    .image-wrap {
+        position: absolute;
+        top:76;
+        left:0;
+        text-align: center;
+        color: white;
+    }
+    .bg-w100-max-height {
+        width:100%;
+    }
+    .centered-text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 40px;
+        text-shadow: -1.5px 0 black, 0 1.5px black, 1.5px 0 black, 0 -1.5px black;
+    }
+</style>
