90ebc3b2024b2ba6bc6722dac5c7d11d9acb5bb1
farid LKHALDOUNI <k.modor@yahoo.com>
2019-07-11 15:14:54 +0200

    add assets css and js to header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta name="csrf-token" content="{{ csrf_token() }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         <title>Farid LKHALDOUNI</title>
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <!-- asset -->
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+        <script src="{{ URL::asset('js/app.js') }} "></script>
+        <!-- CDN -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        
-        @extends('layouts.navbar')
 
-        @yield('content')
+        @include('layouts.navbar')
+
+        <div class="container">
+            @yield('content')
+        </div>
+        
         
